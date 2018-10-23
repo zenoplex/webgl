7d23c2a751a4b7a4db4e8831c2ef2fd0e453dbd5
Gen Ichino <zenoplex@gmail.com>
2018-10-24 01:39:15 +0900

    refactor: fixed samples

--- src/ch02/DrawRectangle.ts
+++ src/ch02/DrawRectangle.ts
@@ -1,10 +1,15 @@
 const main = () => {
-  const canvas: HTMLCanvasElement = document.querySelector('#exmple');
+  const canvas: HTMLCanvasElement | null = document.querySelector('#example');
   if (!canvas) {
-    console.log('Failed to retrieve the <canvas> element');
+    console.warn('Failed to retrieve the <canvas> element');
+    return;
   }
 
   const ctx = canvas.getContext('2d');
+  if (!ctx) {
+    console.warn('Context is null');
+    return;
+  }
   ctx.fillStyle = `rgba(0, 0, 255, 1.0)`;
   ctx.fillRect(120, 10, 150, 150);
 };
