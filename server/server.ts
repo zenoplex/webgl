80dfa70d49dc2e66f719c67ad7230765d3f68d43
Gen Ichino <zenoplex@gmail.com>
2018-10-31 01:11:50 +0900

    refactor: removed server file

--- server/server.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import path from 'path';
-import serveIndex from 'serve-index';
-
-const app = express();
-const outDir = path.resolve(__dirname, '../dist');
-
-app.use(express.static(outDir), serveIndex(outDir));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
