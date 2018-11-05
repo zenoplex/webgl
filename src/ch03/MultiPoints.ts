be307bb0d4a1aa1d7da1b58c050e0383dfd0d4f9
Gen Ichino <zenoplex@gmail.com>
2018-11-06 02:48:58 +0900

    feat: added MultiPoints sample

---
+++ src/ch03/MultiPoints.ts
@@ -0,0 +1,68 @@
+import { getWebGLContext, initShaders } from '../utils/cuonUtils';
+
+const VSHADER_SOURCE = `
+attribute vec4 aPosition;
+void main() {
+  gl_Position = aPosition;
+  gl_PointSize = 10.0;
+}
+`;
+
+const FSHADER_SOURCE = `
+void main() {
+  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}`;
+
+const initVertexBuffers = (
+  gl: WebGLRenderingContext,
+  program: WebGLProgram
+): number => {
+  const verticies = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+  const vertexCount = 3;
+
+  const vertextBuffer = gl.createBuffer();
+  if (!vertextBuffer) {
+    console.error('Failed to create buffer');
+    return -1;
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertextBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticies, gl.STATIC_DRAW);
+
+  const aPosition = gl.getAttribLocation(program, 'aPosition');
+  if (aPosition < 0) {
+    console.error('Failed to get the storage locatoin of aPosition');
+  }
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
+
+  return vertexCount;
+};
+
+export const main = () => {
+  const canvas = document.createElement('canvas');
+  canvas.width = 400;
+  canvas.height = 400;
+  document.body.append(canvas);
+
+  const gl = getWebGLContext(canvas);
+
+  if (!gl) {
+    console.error('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  const program = initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+  if (!program) {
+    console.error('Failed to initialize shaders.');
+    return;
+  }
+
+  const n = initVertexBuffers(gl, program);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.POINTS, 0, n);
+};
+
+main();
