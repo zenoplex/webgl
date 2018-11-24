22127a0d7ea30e9fcfab61788ee1caf22d5a78cf
Gen Ichino <zenoplex@gmail.com>
2018-11-24 18:47:59 +0900

    chore: moved d.ts to typings directory

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,12 +2,12 @@ declare module '*.png' {
   const value: any;
   export = value;
 }
- 
+
 declare module '*.jpg' {
   const value: any;
   export = value;
 }
- 
+
 declare module '*.gif' {
   const value: any;
   export = value;
